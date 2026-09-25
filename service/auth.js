753123b63f9c1eee9f036a87eef0de54b9f1e375
Rahul Chand <chandrahuls2212@gmail.com>
2026-09-25 12:19:15 +0530

    add this to the main project '

--- service/auth.js
+++ service/auth.js
@@ -1,11 +1,11 @@
 const jwt = require("jsonwebtoken");
 const secret = "R18A1H8U21L12";
 function setid(user){
-    const payload = {
+   const payload = {
         _id : user._id,
         email:user.email,
     }
-    return jwt.sign(payload,secret);
+      return jwt.sign(payload,secret); 
 }
 
 function getid(token){
